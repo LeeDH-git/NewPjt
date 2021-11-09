e8635fbd4efbe479303fa255cf03e674db5e69a2
LeeDH-git <djrl02@naver.com>
2021-11-09 14:30:58 +0900

    dd

--- index.js
+++ index.js
@@ -22,9 +22,7 @@ app.get("/", (req, res) => res.send("Hello World!"));
 app.post("/register", (req, res) => {
   // 회원가입 할때 필요한 정보들은 client에 가져오면
   // 그것들을 데이터베이스에 넣어준다.
-
   const user = new User(req.body);
-
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -29,6 +31,24 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+// DB에 저장하기전에 함수 실행
+userSchema.pre("save", function (next) {
+  // DB schema를 가리킴
+  let user = this;
+
+  if (user.isModified("password")) {
+    // 비밀번호를 암호화 시킨다
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  }
 });
 
 const User = mongoose.model("User", userSchema);
