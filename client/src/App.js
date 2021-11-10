570d7a3c761d4b033d6a6121c3d20bf3a58b00db
LeeDH-git <djrl02@naver.com>
2021-11-10 16:32:57 +0900

    d

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import landingPage from "./componets/views/landingPage/LandingPage";
+import loginPage from "./componets/views/loginPage/LoginPage";
+import registerPage from "./componets/views/registerPage/RegisterPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/' componet={landingPage}></Route>
+          <Route exact path='/login' componet={loginPage}></Route>
+          <Route exact path='/register' componet={registerPage}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
